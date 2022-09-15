0247180c14e8589e81e24dd3f2b4e489c14187a9
WrWl <100027250+WrWl@users.noreply.github.com>
2022-09-15 19:51:35 +0300

    /

---
+++ src/fetchCountries.js
@@ -0,0 +1,10 @@
+export function fetchCountries(name){
+    return fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,languages,flags`)
+        .then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json();
+        })
+  .catch((error) => console.log(error))
+}
--- src/index.js
+++ src/index.js
@@ -1,3 +1,52 @@
 import './css/styles.css';
-
+import { fetchCountries } from './fetchCountries';
+const input = document.querySelector("#search-box")
+const countryList = document.querySelector(".country-list")
+const countryInfo = document.querySelector(".country-info")
 const DEBOUNCE_DELAY = 300;
+
+
+function outputCountry(countries) {
+    const markup = countries
+        .map((country) => {
+            return `<div class="country-info">
+        
+        <img src = "${country.flags.svg}" width = 20>
+        <p><b>Name</b>:${country.name.official}</p>
+        <p><b>Capital</b>:${country.capital}</p>
+        <p><b>Population</b>:${country.population}</p>
+        <p><b>Languages</b>:${country.languages}</p>
+        
+        </div>
+        `
+        })
+        .join("");
+  countryInfo.innerHTML = markup;
+} 
+function outputCountries(countries) {
+    const markup = countries
+        .map((country) => {
+            return `<ul class="country-list">
+        <li>
+        <img src = "${country.flags.svg}" width = 20>
+        <p><b>Name</b>:${country.name.official}</p>
+        </li>
+        </ul>
+        `
+        })
+        .join("");
+  countryList.innerHTML = markup;
+} 
+
+
+input.addEventListener("input", _.debounce((country) => {
+    country = input.value
+/*    fetchCountries(country).then() */
+    fetchCountries(country)
+    .then(outputCountries)
+    
+    
+        
+        
+    
+}, DEBOUNCE_DELAY))
